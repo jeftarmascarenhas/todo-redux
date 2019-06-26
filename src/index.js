7547c99cf66ca900ded60338ee6a92be53718c5a
Jeftar Mascarenhas <jeftar.carvalho@cedrotech.com>
2019-06-26 09:48:55 -0300

    router

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux' // react-redux sever para a integração do redux com react
 
 import Todo from './screens/Todo'
-// import App from './screens/App'
+import App from './screens/App'
 import './index.css'
 import store from './redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Todo />
+    <App />
   </Provider>,
   document.getElementById('root'),
 )
